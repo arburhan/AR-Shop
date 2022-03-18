36cc3bf5ce9a29e8d2d1f66a7f3336d09c493c8a
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-03-18 22:04:33 +0600

    bootstrap connect

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -6,6 +6,8 @@ const Product = (props) => {
         <div>
             <img style={{width: 200}} src={props.products.image} alt="" />
             <h2>{props.products.title}</h2>
+            <h3>Category: {props.products.category}</h3>
+            <h2>Price: {props.products.price}$</h2>
         </div>
     );
 };
