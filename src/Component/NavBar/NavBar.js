25fc4562f6c8d4e5443517cd0e58ecf9739cb4f6
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-03 11:39:19 +0600

    set active link:

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Badge } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
     return (
         <div className='nav-tabs'>
-            <Link to='/home'>Home</Link>
-            <Link to='/cart'><i className="bi bi-cart-check-fill cart-icons"></i></Link>
-            <Link to='/about'>About us</Link>
-            <Link to='/contact'>Contact us</Link>
+            <CustomLink to='/home'>Home</CustomLink>
+            <CustomLink to='/cart'><i className="bi bi-cart-check-fill cart-icons"></i></CustomLink>
+            <CustomLink to='/about'>About us</CustomLink>
+            <CustomLink to='/contact'>Contact us</CustomLink>
         </div>
     );
 };
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ textDecoration: match ? "underline" : "none" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
+
 
 export default NavBar;
